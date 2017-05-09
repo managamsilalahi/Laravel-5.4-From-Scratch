cbfbe5d475fbb7a4a41b51dc99eb081decdae790
Managam Silalahi <managam.silalahi@gmail.com>
2017-05-09 21:22:49 +0700

    Make table tasks

--- Working With the Query Builder/blog/database/migrations/2017_05_09_141913_create_tasks_table.php
+++ Working With the Query Builder/blog/database/migrations/2017_05_09_141913_create_tasks_table.php
@@ -15,6 +15,8 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id');
+            $table->text('body');
             $table->timestamps();
         });
     }

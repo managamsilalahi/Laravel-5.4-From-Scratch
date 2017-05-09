fbe1c9821846a674da1e7871914d208ddc96783d
Managam Silalahi <managam.silalahi@gmail.com>
2017-05-09 21:25:07 +0700

    Remove user_id field in tasks table

--- Working With the Query Builder/blog/database/migrations/2017_05_09_141913_create_tasks_table.php
+++ Working With the Query Builder/blog/database/migrations/2017_05_09_141913_create_tasks_table.php
@@ -15,8 +15,7 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
-            $table->text('body');
+            $table->text('body'); 
             $table->timestamps();
         });
     }

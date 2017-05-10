60e73eb07a3988527a9ad8512e291f6f7d6263d9
Managam Silalahi <managam.silalahi@gmail.com>
2017-05-10 20:19:05 +0700

    Complete add Eloquent task model

--- Eloquent 101/blog/routes/web.php
+++ Eloquent 101/blog/routes/web.php
@@ -11,17 +11,21 @@
 |
 */
 
+use App\Task;
+
 Route::get('/', function () {
 	return redirect('/tasks');
 });
 
 Route::get('/tasks', function () {
-	$tasks = DB::table('tasks')->latest()->get();
+	//$tasks = DB::table('tasks')->latest()->get();
+	$tasks = Task::all();
 	return view('tasks.index', compact('tasks'));
 });
 
 Route::get('/tasks/{task}', function ($id) {
-	$task = DB::table('tasks')->find($id);
+	//$task = DB::table('tasks')->find($id);
+	$task = Task::find($id);
 	return view('tasks.show', compact('task'));
 });
 

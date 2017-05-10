93d01cfec4c205dc9252bed8d44287df9e74fade
Managam Silalahi <managam.silalahi@gmail.com>
2017-05-10 20:55:29 +0700

    Change incomplete method to Builder

--- Eloquent 101/blog/app/Task.php
+++ Eloquent 101/blog/app/Task.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    public static function incomplete() {
-    	return static::where('completed', 0)->get();
+    public function scopeIncomplete($query) {
+    	return $query->where('completed', 0);
     }
 }

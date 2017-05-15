52966a601908439b27cfa985a584c913dff058b5
Managam Silalahi <managam.silalahi@gmail.com>
2017-05-15 21:51:03 +0700

    Added views posts.index

--- Layouts and Structure/blog/app/Http/Controllers/PostsController.php
+++ Layouts and Structure/blog/app/Http/Controllers/PostsController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    //
+    public function index() {
+    	return view('posts.index');
+    }
 }
---
+++ Layouts and Structure/blog/resources/views/posts/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+	<h1>Managam Silalahi</h1>
+@endsection('content')
